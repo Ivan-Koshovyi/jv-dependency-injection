a376d218b8468cfe00abd8df90889b91e1b205d2
ivankosh <ivan2023koshovyi@gmail.com>
2025-09-26 19:45:11 +0300

    check-style

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
@@ -17,43 +18,42 @@ public class Injector {
     private Map<Class<?>, Object> instances = new HashMap<>();
     private final Map<Class<?>, Class<?>> interfaceImplementations =
             Map.of(FileReaderService.class, FileReaderServiceImpl.class,
-            ProductService.class, ProductServiceImpl.class,
-            ProductParser.class, ProductParserImpl.class);
+                    ProductService.class, ProductServiceImpl.class,
+                    ProductParser.class, ProductParserImpl.class);
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        Object clazzImplementationInstance = null;
+        if (instances.containsKey(interfaceClazz)) {
+            return instances.get(interfaceClazz);
+        }
         Class<?> clazz = findImplementation(interfaceClazz);
         if (clazz == null) {
-            throw new NullPointerException("Class " + clazz.getName() + " is null");
+            throw new RuntimeException("No implementation found for " + interfaceClazz.getName());
         }
-        if (clazz.isAnnotationPresent(Component.class)) {
-            Field[] declaredFields = clazz.getDeclaredFields();
-            for (Field field : declaredFields) {
-                if (field.isAnnotationPresent(Inject.class)) {
-                    Object fieldInstance = getInstance(field.getType());
+        if (!clazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("This class is unsupported in injector: " + clazz.getName());
+        }
+        Object clazzImplementationInstance = createNewInstance(clazz);
+        instances.put(clazz, clazzImplementationInstance);
 
-                    clazzImplementationInstance = createNewInstance(clazz);
-
-                    try {
-                        field.setAccessible(true);
-                        field.set(clazzImplementationInstance, fieldInstance);
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException("Can`t initialize field value. "
-                                + "Class; " + clazz.getName() + ". Field: " + field.getName());
-                    }
+        // 2. Тепер інжектимо залежності
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field field : declaredFields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                Object fieldInstance = getInstance(field.getType());
+                try {
+                    field.setAccessible(true);
+                    field.set(clazzImplementationInstance, fieldInstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Failed to set field '"
+                            + field.getName() + "' of class " + clazz.getName(), e);
                 }
             }
-            if (clazzImplementationInstance == null) {
-                clazzImplementationInstance = createNewInstance(clazz);
-            }
-            return clazzImplementationInstance;
-        } else {
-            throw new RuntimeException("This class is unsupported in injector");
         }
+        return clazzImplementationInstance;
     }
 
     private Object createNewInstance(Class<?> clazz) {
@@ -67,7 +67,7 @@ public class Injector {
             return instance;
         } catch (InstantiationException | IllegalAccessException
                  | InvocationTargetException | NoSuchMethodException e) {
-            throw new RuntimeException("Can`t create a new instance of " + clazz.getName());
+            throw new RuntimeException("Can't create a new instance of " + clazz.getName(), e);
         }
     }
 
